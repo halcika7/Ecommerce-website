2d1a60c58869ba2d2b383b548c60522249c3258e
Haris Beslic <halcika_7@hotmail.com>
2019-06-13 00:06:22 +0200

    final commit2

--- client/src/users/containers/Product/SharingButtons.js
+++ client/src/users/containers/Product/SharingButtons.js
@@ -20,17 +20,17 @@ const SharingButtons = ({ link }) => {
 	return (
 		<div className="social-links">
 			<label className="Demo__some-network">
-				<FacebookShareButton url={link}>
+				<FacebookShareButton url={link} quote='Halc Store'>
 					<FacebookIcon size={32} round />
 				</FacebookShareButton>
 			</label>
 			<label className="Demo__some-network">
-				<TwitterShareButton url={link}>
+				<TwitterShareButton url={link} title='Halc Store'>
 					<TwitterIcon size={32} round />
 				</TwitterShareButton>
 			</label>
 			<label className="Demo__some-network">
-				<WhatsappShareButton url={link} separator=":: ">
+				<WhatsappShareButton url={link} separator=":: " title="Halc Store">
 					<WhatsappIcon size={32} round />
 				</WhatsappShareButton>
 			</label>
@@ -44,21 +44,22 @@ const SharingButtons = ({ link }) => {
 					url={String(window.location)}
 					media={'sdfdsfsd'}
 					windowWidth={1000}
-					windowHeight={730}>
+					windowHeight={730}
+					description="Halc Store">
 					<PinterestIcon size={32} round />
 				</PinterestShareButton>
 			</label>
 			<label className="Demo__some-network">
 				<RedditShareButton
 					url={link}
-					title={'title'}
+					title={'Halc Store'}
 					windowWidth={660}
 					windowHeight={460}>
 					<RedditIcon size={32} round />
 				</RedditShareButton>
 			</label>
 			<label className="Demo__some-network">
-				<EmailShareButton url={link} subject={'title'} body="body">
+				<EmailShareButton url={link} subject={'Halc Store'} body="body">
 					<EmailIcon size={32} round />
 				</EmailShareButton>
 			</label>
